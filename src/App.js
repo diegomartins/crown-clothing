2287dd55a7599e53c2aa6503ca9a7c14872f8ea3
Diego Martins <diegomartins@github.com>
2020-04-05 17:10:38 +1000

    Updating menu item component and enabling navigation using withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+
+import "./App.css";
+
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
 
 function App() {
     return (
         <div className="App">
-            <HomePage></HomePage>
+            <Switch>
+                <Route exact component={HomePage} path="/"></Route>
+                <Route component={HatsPage} path="/shop/hats"></Route>
+            </Switch>
         </div>
     );
 }
