62709d9fce93fe04f4ca4c79907431975b1f165e
Diego Martins <diegomartins@github.com>
2020-04-06 15:47:44 +1000

    Implementing collection preview and collection items

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-);
 
 function App() {
     return (
         <div className="App">
             <Switch>
                 <Route exact component={HomePage} path="/"></Route>
-                <Route component={HatsPage} path="/shop/hats"></Route>
+                <Route component={ShopPage} path="/shop"></Route>
             </Switch>
         </div>
     );
