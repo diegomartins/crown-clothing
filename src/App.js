135602c85fc0a78c8b35000996f496d4963558f8
Diego Martins <diegomartins@github.com>
2020-04-06 15:56:08 +1000

    Implementing header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
+import Header from "./components/header/header.component";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Switch>
                 <Route exact component={HomePage} path="/"></Route>
                 <Route component={ShopPage} path="/shop"></Route>
