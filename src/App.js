3adde56b52dd1684c64b6cce1517c9400ae7ad54
Diego Martins <diegomartins@github.com>
2020-04-07 18:19:14 +1000

    Creating the Sign up component. Allowing sign up with email and password

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
 import Header from "./components/header/header.component";
 import AuthenticationPage from "./pages/authentication/authentication.component";
-import { auth } from "./firebase/firebase-utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase-utils";
 
 class App extends React.Component {
     unsubscribeFromAuth = null;
@@ -20,9 +20,17 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user });
-            console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: { id: snapshot.id, ...snapshot.data() }
+                    });
+                });
+            }
+
+            this.setState({ currentUser: userAuth });
         });
     }
 
--- src/pages/authentication/authentication.component.jsx
+++ src/pages/authentication/authentication.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./authentication.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const AuthenticationPage = () => (
     <div className="authentication-page">
-        SIGN IN
         <SignIn />
+        <SignUp />
     </div>
 );
 
