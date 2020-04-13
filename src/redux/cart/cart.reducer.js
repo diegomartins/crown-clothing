a6b9b2ff837e58ab97ed2f9895a071024c28120b
Diego Martins <diegomartins@github.com>
2020-04-13 16:30:07 +1000

    Adding the ability to add and remove cart items from the checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,9 @@
 import CartActionTypes from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import {
+    addItemToCart,
+    clearItemFromCart,
+    removeItemFromCart
+} from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,
@@ -13,10 +17,20 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 items: addItemToCart(state.items, action.payload)
             };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                items: removeItemFromCart(state.items, action.payload)
+            };
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                items: clearItemFromCart(state.items, action.payload)
             };
         default:
             return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,22 @@ export const addItemToCart = (cartItems, itemToAdd) => {
 
     return [...cartItems, { ...itemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+    const existingItem = cartItems.find(
+        cartItem => cartItem.id === itemToRemove.id
+    );
+
+    if (existingItem.quantity === 1) {
+        return cartItems.filter(item => item.id !== existingItem.id);
+    }
+
+    return cartItems.map(cartItem =>
+        cartItem.id === itemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    );
+};
+
+export const clearItemFromCart = (cartItems, itemToClear) =>
+    cartItems.filter(item => item.id !== itemToClear.id);
