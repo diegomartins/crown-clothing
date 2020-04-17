eb2390d5589a75902ff5fa0e6fc364585e380718
Diego Martins <diegomartins@github.com>
2020-04-17 16:40:19 +1000

    Moving frontend into client folder, adding express server to handle stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100; // Price in cents
     const publishableKey = "pk_test_UafJkZMhLOIaIsBNNKiRdts800xh1GS1ac";
 
-    const onToken = token => {
-        console.log(token);
-        alert("Payment successful.");
+    const onToken = (token) => {
+        axios({
+            url: "payment",
+            method: "post",
+            data: {
+                amount: priceForStripe,
+                token,
+            },
+        })
+            .then(() => {
+                alert("Payment successful.");
+            })
+            .catch((error) => {
+                console.log("Payment error: ", JSON.parse(error));
+                alert(
+                    "There was an issue with your payment. Please make sure you use the provided test card"
+                );
+            });
     };
 
     return (
